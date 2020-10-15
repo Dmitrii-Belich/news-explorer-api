66047c9ec6b176050c38e14ebb1a5821f7d35bb7
Dmitrii Belich <dim1999@mail.ru>
2020-10-15 23:11:09 +0300

    fix: some fixes

--- controllers/article.js
+++ controllers/article.js
@@ -3,18 +3,18 @@ const CustomError = require('../utils/utils');
 
 module.exports.getArticles = (req, res, next) => {
   Article.find({ owner: req.user._id })
-    .then((Cards) => res.status(200).send(Cards.sort((a, b) => b.createdAt - a.createdAt)))
+    .then((Articles) => res.status(200).send(Articles))
     .catch(next);
 };
 
 module.exports.createArticle = (req, res, next) => {
   const { keyword, title, text, date, source, link, image } = req.body;
   Article.create({ keyword, title, text, date, source, link, image, owner: req.user._id })
-    .then((card) => {
-      Article.findById(card._id)
+    .then((article) => {
+      Article.findById(article._id)
         .orFail(new CustomError(404, 'Данного id нет в базе'))
-        .then((createdCard) => {
-          res.status(200).send(createdCard);
+        .then((createdArticle) => {
+          res.status(200).send(createdArticle);
         });
     }).catch((err) => next(new CustomError(400, err.message)));
 };
@@ -22,13 +22,13 @@ module.exports.createArticle = (req, res, next) => {
 module.exports.deleteArticle = (req, res, next) => {
   Article.findById(req.params.id).populate(['owner'])
     .orFail(new CustomError(404, 'Данного id нет в базе'))
-    .then((card) => {
-      console.log(card)
-      if (card.owner._id.toString() === req.user._id.toString()) {
-        Article.findOneAndDelete({ _id: card._id })
+    .then((article) => {
+      console.log(article)
+      if (article.owner._id.toString() === req.user._id.toString()) {
+        Article.findOneAndDelete({ _id: article._id })
           .orFail(new CustomError(404, 'Данного id нет в базе'))
-          .then((deletedCard) => {
-            res.status(200).send(deletedCard);
+          .then((deletedArticle) => {
+            res.status(200).send(deletedArticle);
           });
       } else {
         throw new CustomError(403, 'У вас нет прав на это действие');
