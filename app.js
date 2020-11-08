e8770365650bf41c53855e5de1d8f37ad0360d0a
Dmitrii Belich <dim1999@mail.ru>
2020-11-08 23:09:30 +0300

    off corse

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 
-const cors = require('cors');
+//const cors = require('cors');
 require('dotenv').config();
 const { errors } = require('celebrate');
 
@@ -15,7 +15,7 @@ const CustomError = require('./utils/utils');
 
 const { PORT = 3000 } = process.env;
 const app = express();
-app.use(cors());
+//app.use(cors());
 app.use(limiter);
 app.use(bodyParser.json());
 
